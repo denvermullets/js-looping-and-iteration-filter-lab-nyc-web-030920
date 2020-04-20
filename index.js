ef7cacc9449cb05b9fe3323fd5b8648cf510000e
devmullets <devmullets@gmail.com>
2020-04-20 17:02:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file
+
+function findMatching(arrayDrivers, name) {
+    // takes array of drivers and a string as args and returns matching list of drivers
+    // case insensitive 
+
+    // let result = arrayDrivers.filter(element => element === nameOfDriver)
+
+    return arrayDrivers.filter(function (driverName) {
+        return driverName.toLowerCase() === name.toLowerCase()
+    })
+}
+
+function fuzzyMatch(arrayDrivers, letterToSearch) {
+    let lengthOfSearch = letterToSearch.length
+    return arrayDrivers.filter(function (letters) {
+        return letters.slice(0,lengthOfSearch) === letterToSearch
+    })
+}
+
+function matchName(arrayDrivers, name) {
+    return arrayDrivers.filter(function (driverName) {
+        return driverName.name.toLowerCase() === name.toLowerCase()
+    })
+
+}
